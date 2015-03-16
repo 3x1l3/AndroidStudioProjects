1a454c2c00e9db7a070f6e637a9f68a2cdfc1a36
Chad <exile@server1.(none)>
2015-03-16 00:07:14 -0600

    updates

--- OMGAndroid/app/src/main/java/com/example/exile/omgandroid/MainActivity.java
+++ OMGAndroid/app/src/main/java/com/example/exile/omgandroid/MainActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,14 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        mainEditText = (EditText) findViewById(R.id.main_textedit);
+
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mNameList);
+
+        mainListView.setAdapter(mArrayAdapter);
 
     }
 
@@ -39,6 +56,10 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        mainTextView.setText("Button clicked!");
+
+
+        mainTextView.setText(  mainEditText.getText().toString() + " is learning android development!");
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
 }
