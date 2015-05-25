c2b6ac0a4516db69cd99a5ee0e21d43eef1419d8
Chad Klassen <chadklassen1@gmail.com>
2015-05-24 20:29:05 -0600

    More

---
+++ WeatherApp/app/src/main/java/com/example/exile/weatherapp/WeatherIcons.java
@@ -0,0 +1,213 @@
+package com.example.exile.weatherapp;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by Exile on 15 _05 _24.
+ */
+public class WeatherIcons {
+
+
+    public static final String day_cloudy_gusts =             "\uuf000";
+    public static final String day_cloudy_windy =             "\uf001";
+    public static final String day_cloudy =                   "\uf002";
+    public static final String day_fog =                      "\uf003";
+    public static final String day_hail =                     "\uf004";
+    public static final String day_lightning =                "\uf005";
+    public static final String day_rain_mix =                 "\uf006";
+    public static final String day_rain_wind =                "\uf007";
+    public static final String day_rain =                     "\uf008";
+    public static final String day_showers =                  "\uf009";
+    public static final String day_snow =                     "\uf00a";
+    public static final String day_sprinkle =                 "\uf00b";
+    public static final String day_sunny_overcast =           "\uf00c";
+    public static final String day_sunny =                    "\uf00d";
+    public static final String day_storm_showers =            "\uf00e";
+    public static final String day_thunderstorm =             "\uf010";
+    public static final String cloudy_gusts =                 "\uf011";
+    public static final String cloudy_windy =                 "\uf012";
+    public static final String cloudy =                       "\uf013";
+    public static final String fog =                          "\uf014";
+    public static final String hail =                         "\uf015";
+    public static final String lightning =                    "\uf016";
+    public static final String rain_mix =                     "\uf017";
+    public static final String rain_wind =                    "\uf018";
+    public static final String rain =                         "\uf019";
+    public static final String showers =                      "\uf01a";
+    public static final String snow =                         "\uf01b";
+    public static final String sprinkle =                     "\uf01c";
+    public static final String storm_showers =                "\uf01d";
+    public static final String thunderstorm =                 "\uf01e";
+    public static final String windy =                        "\uf021";
+    public static final String night_alt_cloudy_gusts =       "\uf022";
+    public static final String night_alt_cloudy_windy =       "\uf023";
+    public static final String night_alt_hail =               "\uf024";
+    public static final String night_alt_lightning =          "\uf025";
+    public static final String night_alt_rain_mix =           "\uf026";
+    public static final String night_alt_rain_wind =          "\uf027";
+    public static final String night_alt_rain =               "\uf028";
+    public static final String night_alt_showers =            "\uf029";
+    public static final String night_alt_snow =               "\uf02a";
+    public static final String night_alt_sprinkle =           "\uf02b";
+    public static final String night_alt_storm_showers =      "\uf02c";
+    public static final String night_alt_thunderstorm =       "\uf02d";
+    public static final String night_clear =                  "\uf02e";
+    public static final String night_cloudy_gusts =           "\uf02f";
+    public static final String night_cloudy_windy =           "\uf030";
+    public static final String night_cloudy =                 "\uf031";
+    public static final String night_hail =                   "\uf032";
+    public static final String night_lightning =              "\uf033";
+    public static final String night_rain_mix =               "\uf034";
+    public static final String night_rain_wind =              "\uf035";
+    public static final String night_rain =                   "\uf036";
+    public static final String night_showers =                "\uf037";
+    public static final String night_snow =                   "\uf038";
+    public static final String night_sprinkle =               "\uf039";
+    public static final String night_storm_showers =          "\uf03a";
+    public static final String night_thunderstorm =           "\uf03b";
+    public static final String celsius =                      "\uf03c";
+    public static final String cloud_down =                   "\uf03d";
+    public static final String cloud_refresh =                "\uf03e";
+    public static final String cloud_up =                     "\uf040";
+    public static final String cloud =                        "\uf041";
+    public static final String degrees =                      "\uf042";
+    public static final String down_left =                    "\uf043";
+    public static final String down =                         "\uf044";
+    public static final String fahrenheit =                   "\uf045";
+    public static final String horizon_alt =                  "\uf046";
+    public static final String horizon =                      "\uf047";
+    public static final String left =                         "\uf048";
+    public static final String _reserved_ =                   "\uf049";
+    public static final String night_fog =                    "\uf04a";
+    public static final String refresh_alt =                  "\uf04b";
+    public static final String refresh =                      "\uf04c";
+    public static final String right =                        "\uf04d";
+    public static final String sprinkles =                    "\uf04e";
+    public static final String strong_wind =                  "\uf050";
+    public static final String sunrise =                      "\uf051";
+    public static final String sunset =                       "\uf052";
+    public static final String thermometer_exterior =         "\uf053";
+    public static final String thermometer_internal =         "\uf054";
+    public static final String thermometer =                  "\uf055";
+    public static final String tornado =                      "\uf056";
+    public static final String up_right =                     "\uf057";
+    public static final String up =                           "\uf058";
+    public static final String wind_west =                    "\uf059";
+    public static final String wind_south_west =              "\uf05a";
+    public static final String wind_south_east =              "\uf05b";
+    public static final String wind_south =                   "\uf05c";
+    public static final String wind_north_west =              "\uf05d";
+    public static final String wind_north_east =              "\uf05e";
+    public static final String wind_north =                   "\uf060";
+    public static final String wind_east =                    "\uf061";
+
+// New v1.2
+    public static final String smoke =                        "\uf062";
+    public static final String dust =                         "\uf063";
+    public static final String snow_wind =                    "\uf064";
+    public static final String day_snow_wind =                "\uf065";
+    public static final String night_snow_wind =              "\uf066";
+    public static final String night_alt_snow_wind =          "\uf067";
+    public static final String day_sleet_storm =              "\uf068";
+    public static final String night_sleet_storm =            "\uf069";
+    public static final String night_alt_sleet_storm =        "\uf06a";
+    public static final String day_snow_thunderstorm =        "\uf06b";
+    public static final String night_snow_thunderstorm =      "\uf06c";
+    public static final String night_alt_snow_thunderstorm =  "\uf06d";
+    public static final String solar_eclipse =                "\uf06e";
+    public static final String lunar_eclipse =                "\uf070";
+    public static final String meteor =                       "\uf071";
+    public static final String hot =                          "\uf072";
+    public static final String hurricane =                    "\uf073";
+    public static final String smog =                         "\uf074";
+    public static final String alien =                        "\uf075";
+    public static final String snowflake_cold =               "\uf076";
+    public static final String stars =                        "\uf077";
+
+// Moon Phases
+//public static final String old_moon_full =                "\uf078";
+//public static final String old_moon_waxing_gibbous =      "\uf079";
+//public static final String old_moon_waxing_quarter =      "\uf07a";
+//public static final String old_moon_waxing_crescent =     "\uf07b";
+//public static final String old_moon_young =               "\uf07c";
+//public static final String old_moon_new =                 "\uf07d";
+//public static final String old_moon_old =                 "\uf07e";
+//public static final String old_moon_waning_crescent =     "\uf080";
+//public static final String old_moon_waning_quarter =      "\uf081";
+//public static final String old_moon_waning_gibbous =      "\uf082";
+
+// More new v1.2
+    public static final String night_partly_cloudy =          "\uf083";
+
+// New 1.3 11_30_14
+    public static final String umbrella                     = "\uf084";
+    public static final String day_windy                    = "\uf085";
+    public static final String night_alt_cloudy          = "\uf086";
+    public static final String up_left                   = "\uf087";
+    public static final String down_right                = "\uf088";
+    public static final String time_12                   = "\uf089";
+    public static final String time_1                    = "\uf08a";
+    public static final String time_2                    = "\uf08b";
+    public static final String time_3                    = "\uf08c";
+    public static final String time_4                    = "\uf08d";
+    public static final String time_5                    = "\uf08e";
+    public static final String time_6                    = "\uf08f";
+    public static final String time_7                    = "\uf090";
+    public static final String time_8                    = "\uf091";
+    public static final String time_9                    = "\uf092";
+    public static final String time_10                   = "\uf093";
+    public static final String time_11                   = "\uf094";
+
+// Moon Phases
+    public static final String moon_new                     = "\uf095";
+    public static final String moon_waxing_cresent_1        = "\uf096";
+    public static final String moon_waxing_cresent_2        = "\uf097";
+    public static final String moon_waxing_cresent_3        = "\uf098";
+    public static final String moon_waxing_cresent_4        = "\uf099";
+    public static final String moon_waxing_cresent_5        = "\uf09a";
+    public static final String moon_waxing_cresent_6        = "\uf09b";
+    public static final String moon_first_quarter           = "\uf09c";
+    public static final String moon_waxing_gibbous_1        = "\uf09d";
+    public static final String moon_waxing_gibbous_2        = "\uf09e";
+    public static final String moon_waxing_gibbous_3        = "\uf09f";
+    public static final String moon_waxing_gibbous_4        = "\uf0a0";
+    public static final String moon_waxing_gibbous_5        = "\uf0a1";
+    public static final String moon_waxing_gibbous_6        = "\uf0a2";
+    public static final String moon_full                    = "\uf0a3";
+    public static final String moon_waning_gibbous_1        = "\uf0a4";
+    public static final String moon_waning_gibbous_2        = "\uf0a5";
+    public static final String moon_waning_gibbous_3        = "\uf0a6";
+    public static final String moon_waning_gibbous_4        = "\uf0a7";
+    public static final String moon_waning_gibbous_5        = "\uf0a8";
+    public static final String moon_waning_gibbous_6        = "\uf0a9";
+    public static final String moon_3rd_quarter             = "\uf0aa";
+    public static final String moon_waning_crescent_1       = "\uf0ab";
+    public static final String moon_waning_crescent_2       = "\uf0ac";
+    public static final String moon_waning_crescent_3       = "\uf0ad";
+    public static final String moon_waning_crescent_4       = "\uf0ae";
+    public static final String moon_waning_crescent_5       = "\uf0af";
+    public static final String moon_waning_crescent_6       = "\uf0b0";
+
+//
+    public static final String wind_default                 = "\uf0b1";
+    public static final String day_sleet                    = "\uf0b2";
+    public static final String night_sleet                  = "\uf0b3";
+    public static final String night_alt_sleet              = "\uf0b4";
+    public static final String sleet                        = "\uf0b5";
+    public static final String day_haze                     = "\uf0b6";
+    public static final String beafort_0                    = "\uf0b7";
+    public static final String beafort_1                    = "\uf0b8";
+    public static final String beafort_2                    = "\uf0b9";
+    public static final String beafort_3                    = "\uf0ba";
+    public static final String beafort_4                    = "\uf0bb";
+    public static final String beafort_5                    = "\uf0bc";
+    public static final String beafort_6                    = "\uf0bd";
+    public static final String beafort_7                    = "\uf0be";
+    public static final String beafort_8                    = "\uf0bf";
+    public static final String beafort_9                    = "\uf0c0";
+    public static final String beafort_10                   = "\uf0c1";
+    public static final String beafort_11                   = "\uf0c2";
+    public static final String beafort_12                   = "\uf0c3";
+
+
+}
